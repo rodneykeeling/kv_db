1500abe229ba627cb8f3a6cc5f3bfa54f1ce5b44
Rodney Keeling <rodneykeeling@gmail.com>
2021-10-10 14:57:54 -0700

    Migrate arg parsing to clap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use clap::{Arg, App};
+
 mod db;
 pub use db::Database;
 
 fn main() {
-    let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().expect("Key was not there");
-    let value = arguments.next().unwrap();
+    let matches = App::new("kvdb")
+        .version("0.1.0")
+        .author("Rodney K. <rodneykeeling@gmail.com>")
+        .subcommand(App::new("insert")
+            .about("Inserts a new key/value")
+            .arg(Arg::new("key")
+                .short('k')
+                .long("key")
+                .takes_value(true))
+            .arg(Arg::new("value")
+                .short('v')
+                .long("value")
+                .takes_value(true)))
+        .get_matches();
 
     let mut database = Database::new().expect("Creating db failed");
-    database.insert(key, value);
+
+    if let Some(matches) = matches.subcommand_matches("insert") {
+        let key = matches.value_of("key").unwrap().to_string();
+        let value = matches.value_of("value").unwrap().to_string();
+
+        database.insert(key, value);
+    }
 }
