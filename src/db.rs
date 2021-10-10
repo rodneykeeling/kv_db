1ad1e59ac262f90903cb292a3de056c3eeeffd59
Rodney Keeling <rodneykeeling@gmail.com>
2021-10-10 14:15:20 -0700

    Move Database code to its own module

---
+++ src/db.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+pub struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    pub fn new() -> Result<Database, std::io::Error> {
+        let mut map = HashMap::new();
+        let mut contents = String::new();
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open("kv.db")
+            .expect("Unable to open file");
+        file.read_to_string(&mut contents)?;
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value!");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+
+        Ok(Database { map })
+    }
+
+    pub fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key.to_lowercase(), value.clone());
+        println!("Insert successful for key '{}' and value '{}'.", key, value);
+    }
+}
+
+// executed after a Database instance goes out of scope but before it's removed
+// from memory
+impl Drop for Database {
+    fn drop(&mut self) {
+        // flush data to file
+        let mut contents = String::new();
+        for (key, value) in &self.map {
+            contents.push_str(key);
+            contents.push('\t');
+            contents.push_str(value);
+            contents.push('\n');
+        }
+        let _ = std::fs::write("kv.db", contents);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-use std::fs::OpenOptions;
-use std::io::prelude::*;
+mod db;
+pub use db::Database;
 
 fn main() {
     let mut arguments = std::env::args().skip(1);
@@ -9,51 +8,4 @@ fn main() {
 
     let mut database = Database::new().expect("Creating db failed");
     database.insert(key, value);
-}
-
-struct Database {
-    map: HashMap<String, String>,
-}
-
-impl Database {
-    fn new() -> Result<Database, std::io::Error> {
-        let mut map = HashMap::new();
-        let mut contents = String::new();
-        let mut file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open("kv.db")
-            .expect("Unable to open file");
-        file.read_to_string(&mut contents)?;
-        for line in contents.lines() {
-            let mut chunks = line.splitn(2, '\t');
-            let key = chunks.next().expect("No key!");
-            let value = chunks.next().expect("No value!");
-            map.insert(key.to_owned(), value.to_owned());
-        }
-
-        Ok(Database { map })
-    }
-
-    fn insert(&mut self, key: String, value: String) {
-        self.map.insert(key.to_lowercase(), value.clone());
-        println!("Insert successful for key '{}' and value '{}'.", key, value);
-    }
-}
-
-// executed after a Database instance goes out of scope but before it's removed
-// from memory
-impl Drop for Database {
-    fn drop(&mut self) {
-        // flush data to file
-        let mut contents = String::new();
-        for (key, value) in &self.map {
-            contents.push_str(key);
-            contents.push('\t');
-            contents.push_str(value);
-            contents.push('\n');
-        }
-        let _ = std::fs::write("kv.db", contents);
-    }
 }
